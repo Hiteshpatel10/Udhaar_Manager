d03b46e1ca9a0a5a8c0b589b3cef2fa47a06e8f4
Hitesh Patel <hpatel.hp2001@gmail.com>
2021-07-10 16:29:16 +0530

    viewmodel create

--- app/src/main/java/com/example/udhaarmanager/main/viewmodel/TransactionViewModel.kt
+++ app/src/main/java/com/example/udhaarmanager/main/viewmodel/TransactionViewModel.kt
@@ -2,6 +2,36 @@ package com.example.udhaarmanager.main.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.udhaarmanager.model.Transaction
+import com.example.udhaarmanager.repo.TransactionRepo
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TransactionViewModel(application: Application): AndroidViewModel(application) {
+class TransactionViewModel @Inject constructor(
+    application: Application,
+    private val transactionRepo: TransactionRepo
+) :
+    AndroidViewModel(application) {
+
+    //insert transaction
+    fun insert(transaction: Transaction) = viewModelScope.launch {
+        transactionRepo.insert(transaction)
+    }
+
+    //delete transaction
+    fun delete(transaction: Transaction) = viewModelScope.launch {
+        transactionRepo.delete(transaction)
+    }
+
+    //update transaction
+    fun update(transaction: Transaction) = viewModelScope.launch {
+        transactionRepo.update(transaction)
+    }
+
+    //get all transaction
+    fun getAllTransaction() = viewModelScope.launch {
+        transactionRepo.getAllTransaction()
+    }
+
 }
---
+++ app/src/main/java/com/example/udhaarmanager/view/DashboardFragment.kt
@@ -0,0 +1,19 @@
+package com.example.udhaarmanager.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.udhaarmanager.R
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
