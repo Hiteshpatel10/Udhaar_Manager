65bc19a4a0b4cc5f7690f35d8ce4d02ad1396dce
Hitesh Patel <hpatel.hp2001@gmail.com>
2021-08-05 09:32:41 +0530

    drawables deleted and auth ui optimized

--- app/src/main/java/com/example/udhaarmanager/auth/AuthActivity.kt
+++ app/src/main/java/com/example/udhaarmanager/auth/AuthActivity.kt
@@ -2,21 +2,14 @@ package com.example.udhaarmanager.auth
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import com.example.udhaarmanager.R
-import com.example.udhaarmanager.databinding.ActivityAuthBinding
 import com.example.udhaarmanager.main.MainActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class AuthActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAuthBinding
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/udhaarmanager/auth/EmailVerificationFragment.kt
+++ app/src/main/java/com/example/udhaarmanager/auth/EmailVerificationFragment.kt
@@ -2,12 +2,11 @@ package com.example.udhaarmanager.auth
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.udhaarmanager.R
+import androidx.fragment.app.Fragment
 import com.example.udhaarmanager.databinding.FragmentEmailVerificationBinding
 import com.example.udhaarmanager.main.MainActivity
 import com.google.firebase.auth.ktx.auth
